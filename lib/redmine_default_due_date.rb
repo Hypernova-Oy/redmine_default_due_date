6ec951605aad253ba47120c236f420927539e66c
Your Name <you@example.com>
2024-10-19 23:10:06 +0000

    Redmine 5 support

--- lib/redmine_default_due_date.rb
+++ lib/redmine_default_due_date.rb
@@ -1,18 +1,15 @@
+module RedmineDefaultDueDate
+  class DefaultDueDateListener < Redmine::Hook::ViewListener
 
-module Redmine
-  module Plugins
-    class DefaultDueDateListener < Redmine::Hook::ViewListener
+    DEFAULT_DUE_DATE = 7
 
-      DEFAULT_DUE_DATE = 7
-
-      def view_issues_new_top(context)
-        context[:issue].due_date ||= self.class.default_due_date
-      end
-
-      def self.default_due_date
-        Date.today + Setting.plugin_redmine_default_due_date['default_due_date'].to_i.days
-      end
-
+    def view_issues_new_top(context)
+      context[:issue].due_date ||= self.class.default_due_date
     end
+
+    def self.default_due_date
+      Date.today + Setting.plugin_redmine_default_due_date['default_due_date'].to_i.days
+    end
+
   end
 end
